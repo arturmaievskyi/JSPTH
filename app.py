7986a1e6768e4c198c449de742ab2be2b59a6772
arturmaievskyi <arturmaievskyi1@gmail.com>
2024-10-13 19:16:01 +0200

    b

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from JSPTH.ClasesCode import Console
-from JSPTH.Math.MainMath import Math
+from JSPTH.MainMath import Math
 
 test_a = (Console.IntGet("First num "))
 test_b = (Console.IntGet("Second num "))
