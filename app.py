77e5411c504248155c8d803a080ebc62cce009e7
arturmaievskyi <arturmaievskyi1@gmail.com>
2024-10-15 21:19:50 +0200

    int

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from JSPTH.ClasesCode import Console
 from JSPTH.Math.MainMath import MyMath as Math
 
-test_a = (Console.IntGet("First num "))
-test_b = (Console.IntGet("Second num "))
+test_a = int(Console.IntGet("First num "))
+test_b = int(Console.IntGet("Second num "))
 
 Math.add(test_a, test_b)
 Math.square_number(test_a)
