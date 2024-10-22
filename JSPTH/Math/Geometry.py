da62ddff1c8b8ade07dc7fea1bea72e9094bfabe
arturmaievskyi <arturmaievskyi1@gmail.com>
2024-10-22 19:12:01 +0200

    s

--- JSPTH/Math/Geometry.py
+++ JSPTH/Math/Geometry.py
@@ -159,14 +159,14 @@ class GeometryMath():
         return 4 * math.pi * radius ** 2
 
 
-def sphere_volume(radius: float) -> float:
-    """
-    Calculate the volume of a sphere.
-    
-    Parameters:
-    radius (float): The radius of the sphere.
-    
-    Returns:
-    float: The volume of the sphere.
-    """
-    return (4/3) * math.pi * radius ** 3
+    def sphere_volume(radius: float) -> float:
+        """
+        Calculate the volume of a sphere.
+        
+        Parameters:
+        radius (float): The radius of the sphere.
+        
+        Returns:
+        float: The volume of the sphere.
+        """
+        return (4/3) * math.pi * radius ** 3
