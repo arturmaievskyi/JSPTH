bc33b23dc3366f7f7587eee35cede5967a8d308c
arturmaievskyi <arturmaievskyi1@gmail.com>
2024-10-25 20:42:48 +0200

    sss

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import { Calculator } from "./JSPTH/MathJS/MainMath";
 
-let calc = Calculator
-let test = calc.add(2, 2)
+const { Calculator } = require('./JSPATH/MathJS/MainMath');
+const { Trigonometry } = require('./JSPATH/MathJS/trigonometry');
+let test = Calculator.add(2, 2)
 console.log(test);
