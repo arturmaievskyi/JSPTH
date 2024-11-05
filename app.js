21fc9920d85f581c150dc0c94db4807d096c90e1
arturmaievskyi <arturmaievskyi1@gmail.com>
2024-11-05 17:19:55 +0100

    ggg

--- app.js
+++ app.js
@@ -8,3 +8,4 @@ console.log(Calculator.prototype.subtract(10, 2))
 console.log(Calculator.prototype.multiply(5, 5))
 console.log(Calculator.prototype.factorial(9))
 console.log(Calculator.prototype.square(5))
+
