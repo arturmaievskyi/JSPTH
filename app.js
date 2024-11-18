4ebfc426b03ce639c2831548bbdd43697f048663
arturmaievskyi <arturmaievskyi1@gmail.com>
2024-11-18 20:05:28 +0100

    s

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 import {Calculator} from "./JSPTH/MathJS/MainMath";
 
 
-console.log(Calculator.prototype.add(10, 20))
-console.log(Calculator.prototype.root(9));
-console.log(Calculator.prototype.divide(10, 2))
-console.log(Calculator.prototype.subtract(10, 2))
-console.log(Calculator.prototype.multiply(5, 5))
-console.log(Calculator.prototype.factorial(9))
-console.log(Calculator.prototype.square(5))
+new calc = Calculator()
 
