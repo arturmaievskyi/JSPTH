b8b5b1623d1ebaf9ebd7c1d4b2f51fcdf5d6cbab
arturmaievskyi1 <arturmaievskyi1@gmail.com>
2024-12-02 07:08:58 +0100

    x

---
+++ Java/src/JSJ/Convertors/VolumeConvertor.java
@@ -0,0 +1,18 @@
+public class VolumeConverter {
+
+    private static final double LITER_TO_ML = 1000;
+    private static final double LITER_TO_CUBIC_METER = 0.001;
+    private static final double LITER_TO_GALLON = 0.264172;
+
+    public static double litersToMilliliters(double liters) {
+        return liters * LITER_TO_ML;
+    }
+
+    public static double litersToCubicMeters(double liters) {
+        return liters * LITER_TO_CUBIC_METER;
+    }
+
+    public static double litersToGallons(double liters) {
+        return liters * LITER_TO_GALLON;
+    }
+}
