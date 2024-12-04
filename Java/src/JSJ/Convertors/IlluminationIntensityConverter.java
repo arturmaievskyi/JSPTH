74bd4d88f8843608c71b8e07e124abe0d9dfc989
arturmaievskyi1 <arturmaievskyi1@gmail.com>
2024-12-04 22:49:12 +0100

    s

--- Java/src/JSJ/Convertors/IlluminationIntensityConverter.java
+++ Java/src/JSJ/Convertors/IlluminationIntensityConverter.java
@@ -1,4 +1,16 @@
-package src.JSJ.Convertors;
+package JSJ.Convertors;
 
+// File: IlluminationIntensityConverter.java
 public class IlluminationIntensityConverter {
+
+    private static final double LUX_TO_FOOT_CANDLE = 0.092903;
+
+    public static double luxToFootCandles(double lux) {
+        return lux * LUX_TO_FOOT_CANDLE;
+    }
+
+    public static double footCandlesToLux(double footCandles) {
+        return footCandles / LUX_TO_FOOT_CANDLE;
+    }
+
 }
