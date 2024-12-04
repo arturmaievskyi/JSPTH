1928a175aa2b5c3cd3834e9bb1ef03c2ed26855c
arturmaievskyi1 <arturmaievskyi1@gmail.com>
2024-12-04 22:48:10 +0100

    s

--- Java/src/JSJ/Convertors/CapacitanceConverter.java
+++ Java/src/JSJ/Convertors/CapacitanceConverter.java
@@ -1,4 +1,23 @@
-package src.JSJ.Convertors;
+package JSJ.Convertors;
 
+// File: CapacitanceConverter.java
 public class CapacitanceConverter {
+
+    private static final double FARAD_TO_MICROFARAD = 1e6;
+    private static final double FARAD_TO_NANOFARAD = 1e9;
+    private static final double FARAD_TO_PICOFARAD = 1e12;
+
+    public static double faradsToMicrofarads(double farads) {
+        return farads * FARAD_TO_MICROFARAD;
+    }
+
+    public static double faradsToNanofarads(double farads) {
+        return farads * FARAD_TO_NANOFARAD;
+    }
+
+    public static double faradsToPicofarads(double farads) {
+        return farads * FARAD_TO_PICOFARAD;
+    }
+
+
 }
