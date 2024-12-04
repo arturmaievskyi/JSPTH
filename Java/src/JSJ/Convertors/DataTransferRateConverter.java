6f141994e7613133f028057885597c6aabf831cb
arturmaievskyi1 <arturmaievskyi1@gmail.com>
2024-12-04 22:47:07 +0100

    s

--- Java/src/JSJ/Convertors/DataTransferRateConverter.java
+++ Java/src/JSJ/Convertors/DataTransferRateConverter.java
@@ -1,4 +1,22 @@
-package src.JSJ.Convertors;
+package JSJ.Convertors;
 
+// File: DataTransferRateConverter.java
 public class DataTransferRateConverter {
+
+    private static final double BPS_TO_KBPS = 0.001;
+    private static final double BPS_TO_MBPS = 1e-6;
+    private static final double BPS_TO_GBPS = 1e-9;
+
+    public static double bitsToKilobits(double bps) {
+        return bps * BPS_TO_KBPS;
+    }
+
+    public static double bitsToMegabits(double bps) {
+        return bps * BPS_TO_MBPS;
+    }
+
+    public static double bitsToGigabits(double bps) {
+        return bps * BPS_TO_GBPS;
+    }
+
 }
--- Java/src/JSJ/Convertors/RadioactivityConverter.java
+++ Java/src/JSJ/Convertors/RadioactivityConverter.java
@@ -1,4 +1,17 @@
-package src.JSJ.Convertors;
+package JSJ.Convertors;
 
+
+// File: RadioactivityConverter.java
 public class RadioactivityConverter {
+
+    private static final double BQ_TO_CI = 2.7e-11;
+    private static final double BQ_TO_RD = 1e6;
+
+    public static double becquerelsToCuries(double bq) {
+        return bq * BQ_TO_CI;
+    }
+
+    public static double becquerelsToRutherfords(double bq) {
+        return bq / BQ_TO_RD;
+    }
 }
