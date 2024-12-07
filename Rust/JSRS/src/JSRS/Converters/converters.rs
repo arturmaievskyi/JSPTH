c7ca3a078c1d6dd22428f23f9401d86b658504ba
arturmaievskyi1 <arturmaievskyi1@gmail.com>
2024-12-07 21:13:29 +0100

    a

--- Rust/JSRS/src/JSRS/Converters/converters.rs
+++ Rust/JSRS/src/JSRS/Converters/converters.rs
@@ -268,3 +268,202 @@ impl IlluminationIntensityConverter {
         fc / 0.092903
     }
 }
+
+struct DistanceConverter;
+
+impl DistanceConverter {
+    fn kilometers_to_miles(km: f64) -> f64 {
+        km * 0.621371
+    }
+
+    fn miles_to_kilometers(miles: f64) -> f64 {
+        miles / 0.621371
+    }
+
+    fn meters_to_feet(meters: f64) -> f64 {
+        meters * 3.28084
+    }
+
+    fn feet_to_meters(feet: f64) -> f64 {
+        feet / 3.28084
+    }
+}
+
+struct TemperatureConverter;
+
+impl TemperatureConverter {
+    fn celsius_to_fahrenheit(celsius: f64) -> f64 {
+        celsius * 9.0 / 5.0 + 32.0
+    }
+
+    fn fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
+        (fahrenheit - 32.0) * 5.0 / 9.0
+    }
+
+    fn celsius_to_kelvin(celsius: f64) -> f64 {
+        celsius + 273.15
+    }
+
+    fn kelvin_to_celsius(kelvin: f64) -> f64 {
+        kelvin - 273.15
+    }
+}
+
+struct PressureConverter;
+
+impl PressureConverter {
+    fn pascals_to_atmospheres(pascals: f64) -> f64 {
+        pascals / 101325.0
+    }
+
+    fn atmospheres_to_pascals(atm: f64) -> f64 {
+        atm * 101325.0
+    }
+
+    fn psi_to_pascals(psi: f64) -> f64 {
+        psi * 6894.76
+    }
+
+    fn pascals_to_psi(pascals: f64) -> f64 {
+        pascals / 6894.76
+    }
+}
+
+struct EnergyConverter;
+
+impl EnergyConverter {
+    fn joules_to_calories(joules: f64) -> f64 {
+        joules / 4.184
+    }
+
+    fn calories_to_joules(calories: f64) -> f64 {
+        calories * 4.184
+    }
+
+    fn kilowatt_hours_to_joules(kwh: f64) -> f64 {
+        kwh * 3.6e6
+    }
+
+    fn joules_to_kilowatt_hours(joules: f64) -> f64 {
+        joules / 3.6e6
+    }
+}
+
+struct FrequencyConverter;
+
+impl FrequencyConverter {
+    fn hertz_to_kilohertz(hz: f64) -> f64 {
+        hz / 1000.0
+    }
+
+    fn kilohertz_to_hertz(khz: f64) -> f64 {
+        khz * 1000.0
+    }
+
+    fn hertz_to_megahertz(hz: f64) -> f64 {
+        hz / 1e6
+    }
+
+    fn megahertz_to_hertz(mhz: f64) -> f64 {
+        mhz * 1e6
+    }
+}
+struct StorageConverter;
+
+impl StorageConverter {
+    fn bytes_to_kilobytes(bytes: f64) -> f64 {
+        bytes / 1024.0
+    }
+
+    fn kilobytes_to_bytes(kb: f64) -> f64 {
+        kb * 1024.0
+    }
+
+    fn megabytes_to_gigabytes(mb: f64) -> f64 {
+        mb / 1024.0
+    }
+
+    fn gigabytes_to_megabytes(gb: f64) -> f64 {
+        gb * 1024.0
+    }
+}
+
+struct AreaConverter;
+
+impl AreaConverter {
+    fn square_meters_to_square_feet(sq_meters: f64) -> f64 {
+        sq_meters * 10.7639
+    }
+
+    fn square_feet_to_square_meters(sq_feet: f64) -> f64 {
+        sq_feet / 10.7639
+    }
+
+    fn hectares_to_acres(hectares: f64) -> f64 {
+        hectares * 2.47105
+    }
+
+    fn acres_to_hectares(acres: f64) -> f64 {
+        acres / 2.47105
+    }
+}
+
+struct VolumeConverter;
+
+impl VolumeConverter {
+    fn liters_to_gallons(liters: f64) -> f64 {
+        liters * 0.264172
+    }
+
+    fn gallons_to_liters(gallons: f64) -> f64 {
+        gallons / 0.264172
+    }
+
+    fn cubic_meters_to_cubic_feet(cubic_meters: f64) -> f64 {
+        cubic_meters * 35.3147
+    }
+
+    fn cubic_feet_to_cubic_meters(cubic_feet: f64) -> f64 {
+        cubic_feet / 35.3147
+    }
+}
+
+struct SpeedConverter;
+
+impl SpeedConverter {
+    fn kilometers_per_hour_to_miles_per_hour(kph: f64) -> f64 {
+        kph * 0.621371
+    }
+
+    fn miles_per_hour_to_kilometers_per_hour(mph: f64) -> f64 {
+        mph / 0.621371
+    }
+
+    fn meters_per_second_to_kilometers_per_hour(mps: f64) -> f64 {
+        mps * 3.6
+    }
+
+    fn kilometers_per_hour_to_meters_per_second(kph: f64) -> f64 {
+        kph / 3.6
+    }
+}
+
+struct MassConverter;
+
+impl MassConverter {
+    fn kilograms_to_pounds(kg: f64) -> f64 {
+        kg * 2.20462
+    }
+
+    fn pounds_to_kilograms(pounds: f64) -> f64 {
+        pounds / 2.20462
+    }
+
+    fn grams_to_ounces(grams: f64) -> f64 {
+        grams * 0.035274
+    }
+
+    fn ounces_to_grams(ounces: f64) -> f64 {
+        ounces / 0.035274
+    }
+}
